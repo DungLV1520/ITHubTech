d1d8a32e5c0cad2bb20ade610d713a4671699bd2
Lường Dũng <ole.phan.10@gmail.com>
2023-04-06 00:04:56 +0700

    update click

--- src/app/components/pages/home/home.service.ts
+++ src/app/components/pages/home/home.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from "@angular/core";
 
 @Injectable({ providedIn: "root" })
 export class HomeService {
-  GlobalComponent = "https://ulink.asia/api/google/log";
+  GlobalComponent = "https://ulink.asia/api/google/ithub/log";
 
   constructor(private http: HttpClient) {}
 
   setParam(id: number) {
-    return this.http.get(`${this.GlobalComponent}?a=${id}`);
+    return this.http.get(`${this.GlobalComponent}?a=${id}`, {
+      responseType: "text",
+    });
   }
 }
