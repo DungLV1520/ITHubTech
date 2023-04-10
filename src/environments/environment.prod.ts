e4937a151a5c17d6f8c6d2f29c4e317a8e85dcbd
Lường Dũng <ole.phan.10@gmail.com>
2023-04-10 10:03:09 +0700

    init ver bybit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA4tT_xYgRpOf1bul3QzrFqvLxjqpJXXUE",
-    authDomain: "ithub-2009b.firebaseapp.com",
-    projectId: "ithub-2009b",
-    storageBucket: "ithub-2009b.appspot.com",
-    messagingSenderId: "1063056108774",
-    appId: "1:1063056108774:web:96444b3e0dcb79f20a5f27",
+    apiKey: "AIzaSyDScw9L2A2EVxZhYmpRi-5mfB97URWCVv8",
+    authDomain: "ithub-bybit.firebaseapp.com",
+    projectId: "ithub-bybit",
+    storageBucket: "ithub-bybit.appspot.com",
+    messagingSenderId: "467084432383",
+    appId: "1:467084432383:web:073d1f8b0997dc9a6b0dca",
   },
 };
