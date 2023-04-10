79430140f4e5f445b89bf990c5e0a5c3981a583c
Lường Dũng <ole.phan.10@gmail.com>
2023-04-11 00:05:47 +0700

    update code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDScw9L2A2EVxZhYmpRi-5mfB97URWCVv8",
-    authDomain: "ithub-bybit.firebaseapp.com",
-    projectId: "ithub-bybit",
-    storageBucket: "ithub-bybit.appspot.com",
-    messagingSenderId: "467084432383",
-    appId: "1:467084432383:web:073d1f8b0997dc9a6b0dca",
+    apiKey: "AIzaSyAAhklBOIGqP2gLGCpSCUc3qbMpRV68Jkw",
+    authDomain: "ithub-1.firebaseapp.com",
+    projectId: "ithub-1",
+    storageBucket: "ithub-1.appspot.com",
+    messagingSenderId: "917120279730",
+    appId: "1:917120279730:web:ec3e8520f23bc73a4395ab",
   },
 };
