4935b91f6eceff187ffe535bff21c2ca9c7e8a10
Lường Dũng <ole.phan.10@gmail.com>
2023-04-10 10:20:49 +0700

    update code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA4tT_xYgRpOf1bul3QzrFqvLxjqpJXXUE",
-    authDomain: "ithub-2009b.firebaseapp.com",
-    projectId: "ithub-2009b",
-    storageBucket: "ithub-2009b.appspot.com",
-    messagingSenderId: "1063056108774",
-    appId: "1:1063056108774:web:96444b3e0dcb79f20a5f27",
+    apiKey: "AIzaSyDMDjSyCvh7eD7qLW5RGLhcQafWrS6NOa8",
+    authDomain: "ithub-91926.firebaseapp.com",
+    projectId: "ithub-91926",
+    storageBucket: "ithub-91926.appspot.com",
+    messagingSenderId: "308605604296",
+    appId: "1:308605604296:web:1183853272288f5833f0fd",
+    measurementId: "G-Y6YJM54RJ3",
   },
 };
