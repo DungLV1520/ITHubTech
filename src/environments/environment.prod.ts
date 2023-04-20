6301fe3d15a6f5580b06f21919435cce6c0bb2ca
Lường Dũng <ole.phan.10@gmail.com>
2023-04-20 13:39:10 +0700

    init ithub-pub-adsterra

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyC53ibaAVfgRdBfKJcIgL0rk5up4oCzHes",
-    authDomain: "ithub-adsterra.firebaseapp.com",
-    projectId: "ithub-adsterra",
-    storageBucket: "ithub-adsterra.appspot.com",
-    messagingSenderId: "1053425407212",
-    appId: "1:1053425407212:web:8c8f9e838d9c6ab15f81c9",
+    apiKey: "AIzaSyC1q4J27vRLHln1D5RCRc4ILOzOhkGRaDM",
+    authDomain: "ithub-pub-adsterra.firebaseapp.com",
+    projectId: "ithub-pub-adsterra",
+    storageBucket: "ithub-pub-adsterra.appspot.com",
+    messagingSenderId: "544372764490",
+    appId: "1:544372764490:web:a09bd158a8160edb009bb3",
   },
 };
