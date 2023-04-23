c1d005acce22b6133770642ed341a52abcd72da0
Lường Dũng <ole.phan.10@gmail.com>
2023-04-23 18:27:25 +0700

    init project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC1q4J27vRLHln1D5RCRc4ILOzOhkGRaDM",
-    authDomain: "ithub-pub-adsterra.firebaseapp.com",
-    projectId: "ithub-pub-adsterra",
-    storageBucket: "ithub-pub-adsterra.appspot.com",
-    messagingSenderId: "544372764490",
-    appId: "1:544372764490:web:a09bd158a8160edb009bb3",
+    apiKey: "AIzaSyC2fB0DhgSjPH93sIbhm5WmKzi1eKAc0Kk",
+    authDomain: "ithub-789club.firebaseapp.com",
+    projectId: "ithub-789club",
+    storageBucket: "ithub-789club.appspot.com",
+    messagingSenderId: "2191222995",
+    appId: "1:2191222995:web:1e37087da0aaf0b5bd0c26",
   },
 };
 
