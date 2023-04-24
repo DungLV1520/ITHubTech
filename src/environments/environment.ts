e7ac8dc03d3222da9d163de36e9bde0a53eb5e37
Lường Dũng <ole.phan.10@gmail.com>
2023-04-24 08:07:48 +0700

    change branch final

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyC2fB0DhgSjPH93sIbhm5WmKzi1eKAc0Kk",
-    authDomain: "ithub-789club.firebaseapp.com",
-    projectId: "ithub-789club",
-    storageBucket: "ithub-789club.appspot.com",
-    messagingSenderId: "2191222995",
-    appId: "1:2191222995:web:1e37087da0aaf0b5bd0c26",
-  },
 };
 
 /*
